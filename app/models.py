f65b839588d15cbbbd43494ba2171941da2d6751
Mateus Volpato Cirico <teteuvolpato@gmail.com>
2025-01-09 14:56:44 -0300

    Alteracao no models de cliente e profissional

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class Cliente(models.Model):
     sobrenome = models.CharField(max_length=70)
     data_nascimento = models.DateField()
     cep = models.CharField(max_length=10)
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
 
 
 class Profissional(models.Model):
@@ -13,7 +15,12 @@ class Profissional(models.Model):
     sobrenome = models.CharField(max_length=70)
     data_nascimento = models.DateField()
     cep = models.CharField(max_length=10)
+    area_atuacao = models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome} {(self.area_atuacao)}"
 
 
 class CustomUser(AbstractUser):
     is_admin = models.BooleanField(default=False)
+    is_profissional = models.BooleanField(default=False)
+    is_cliente = models.BooleanField(default=False)
